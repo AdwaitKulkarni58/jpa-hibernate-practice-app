9dba3961b593ed4953e760d6f156b5a1a03309d9
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2022-11-18 09:13:23 -0800

    Add course model class

--- src/main/java/com/springboot/jpaandhibernate/jdbc/JdbcRepository.java
+++ src/main/java/com/springboot/jpaandhibernate/jdbc/JdbcRepository.java
@@ -6,10 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.springboot.jpaandhibernate.jdbc.course.Course;
+
 @Repository
 public class JdbcRepository {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+
+	public void insert(Course course) {
+		jdbcTemplate.update(course);
+	}
 
 }
