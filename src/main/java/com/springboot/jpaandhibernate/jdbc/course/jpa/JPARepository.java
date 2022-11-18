da25b7a41984caa59eb41d85cce806a32ad0a80f
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2022-11-18 10:47:45 -0800

    Add insert method for jpa

--- src/main/java/com/springboot/jpaandhibernate/jdbc/course/jpa/JPARepository.java
+++ src/main/java/com/springboot/jpaandhibernate/jdbc/course/jpa/JPARepository.java
@@ -1,5 +1,19 @@
 package com.springboot.jpaandhibernate.jdbc.course.jpa;
 
+import org.springframework.stereotype.Repository;
+
+import com.springboot.jpaandhibernate.jdbc.course.Course;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
 public class JPARepository {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void insert(Course course) {
+		entityManager.merge(course);
+	}
 }
